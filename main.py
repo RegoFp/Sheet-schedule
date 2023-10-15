8afbbd4927fcf0f52c128baf600ced379bb60f38
RegoFp <92778484+RegoFp@users.noreply.github.com>
2023-10-15 19:31:52 +0200

    Added EE and emiru schedules

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+import time
 from dataclasses import dataclass
 from datetime import datetime
 
@@ -12,8 +14,6 @@ from icalevents.icalevents import events
 
 from dateutil import parser
 
-calendar_path = "calendar.ics"
-
 
 @dataclass
 class Stream:
@@ -24,7 +24,6 @@ class Stream:
 def read_day(box, sh):
     time_str = sh.sheet1.cell(box + 1, 5).value
     date_object = parser.parse(time_str)
-
 
     date_str = sh.sheet1.cell(box, 2).value  # month/day
     month = int(date_str.split("/")[0])
@@ -46,7 +45,7 @@ def read_day(box, sh):
     return stream
 
 
-def read_schedule(cal):
+def read_miz_schedule(cal):
     gc = gspread.service_account(filename="service_account.json")
 
     sh = gc.open_by_url(
@@ -66,8 +65,100 @@ def read_schedule(cal):
     return cal
 
 
+def read_ee_schedule(cal):
+    gc = gspread.service_account(filename="service_account.json")
+
+    sh = gc.open_by_url(
+        'https://docs.google.com/spreadsheets/d/1hJ5FJCXeaTRPb8ROR-PiEzMYKb3U7Z35ujn6mSzzHKg/edit#gid=0')
+
+    date_regex = re.compile(r'(\w+) (\d+)\w+ \| (\w+) \| (\d+):(\d+)\w+ (\w+)')
+
+    # Searches for all boxes with a date
+    cells = sh.get_worksheet(0).findall(date_regex, in_column=4)
+
+    for cell in cells:
+        print(cell.value)
+        date_string = cell.value.split("|")
+
+        date_string = date_string[0][:-3] + date_string[2]
+        print(date_string)
+        format_string = "%B %d %I:%M%p %Z"
+
+        datetime_object = parser.parse(date_string)
+
+        title_string = sh.get_worksheet(0).cell(cell.row + 3, 4).value
+
+        description_string = sh.get_worksheet(0).cell(cell.row + 5, 4).value
+
+        if description_string is None:
+            description_string = sh.get_worksheet(0).cell(cell.row + 6, 4).value
+            collab_url = sh.get_worksheet(0).cell(cell.row + 4, 4).value
+
+            description_string = collab_url + "\n" + description_string
+
+        event = Event()
+        event.add('summary', title_string)
+        event.add('description', description_string)
+        event.add('dtstart', datetime_object, parameters={'TZID': 'US/Central'})
+
+        can_add = True
+
+        for compontent in cal.walk("VEVENT"):
+            if compontent["summary"] == event["summary"]:
+                can_add = False
+                # compontent['status'] = vText('CANCELLED')
+
+        if can_add:
+            cal.add_component(event)
+
+    return cal
+
+
+def read_emiru_schedule():
+    calendar_path = "Emiru_calendar.ics"
+
+    calendar = get_calendar(calendar_path)
+
+    gc = gspread.service_account(filename="service_account.json")
+
+    sh = gc.open_by_url(
+        'https://docs.google.com/spreadsheets/d/1WFuxI2R5iLzt7x0k1LVV9Te5uQEb2X6CS5vc_VYC-AQ/edit#gid=2084945952')
+
+    regex = re.compile(r"\d{2}/\d{2}")
+
+    cells = sh.get_worksheet(0).findall(regex, in_column=2)
+
+    for cell in cells:
+        datetime_object = parser.parse(cell.value + " " + sh.get_worksheet(0).cell(cell.row + 1, cell.col + 3).value)
+
+        print(datetime_object)
+        title_string = sh.get_worksheet(0).cell(cell.row, cell.col + 3).value
+
+        description_string = sh.get_worksheet(0).cell(cell.row + 2, cell.col + 3).value
+
+        event = Event()
+        event.add('summary', title_string)
+        event.add('description', description_string)
+        event.add('dtstart', datetime_object, parameters={'TZID': 'US/Central'})
+
+        if "❌" in title_string:
+            event['status'] = vText('CANCELLED')
+
+        can_add = True
+
+        # TODO turn this into a method, since ee and emiru use it
+        for compontent in calendar.walk("VEVENT"):
+            if compontent["summary"] == event["summary"]:
+                can_add = False
+
+        if can_add:
+            calendar.add_component(event)
+
+    save(calendar, calendar_path)
+
+
 # Reads the calendar
-def get_calendar():
+def get_calendar(calendar_path):
     calendar = Calendar()
 
     if not exists(calendar_path):
@@ -85,7 +176,7 @@ def get_calendar():
     for event in old_calendar.walk('VEVENT'):
         start_date = event.get("dtstart").dt.date()
         # Check if it matches the target date
-        if start_date <= datetime.today().date():
+        if start_date < datetime.today().date():
             # Print the summary of the event
             print(event.get("summary"))
             # Delete the event from the calendar
@@ -95,7 +186,7 @@ def get_calendar():
 
 
 # Saves the calendar
-def save(cal):
+def save(cal, calendar_path):
     if exists(calendar_path):
         os.remove(calendar_path)
 
@@ -104,13 +195,31 @@ def save(cal):
     f.close()
 
 
+def get_miz_schedule():
+    calendar_path = "calendar.ics"
+
+    cal = get_calendar(calendar_path)
+
+    cal = read_miz_schedule(cal)
+
+    save(cal, calendar_path)
+
+
+def get_ee_schedule():
+    calendar_path = "EE_calendar.ics"
+
+    calendar = get_calendar(calendar_path)
+
+    calendar = read_ee_schedule(calendar)
+
+    save(calendar, calendar_path)
+
+
 if __name__ == '__main__':
+    get_miz_schedule()
+    time.sleep(100)
 
-    cal = get_calendar()
+    get_ee_schedule()
+    time.sleep(100)
 
-    for component in cal.walk():
-        print(component.name)
-
-    cal = read_schedule(cal)
-
-    save(cal)
+    read_emiru_schedule()
